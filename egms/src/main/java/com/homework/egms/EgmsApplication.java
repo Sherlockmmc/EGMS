2da56cd557fe38a7debd27847099ce56fe3b93c7
Sherlockmmc <731853112@qq.com>
2020-06-26 11:31:55 +0800

    First commit

---
+++ egms/src/main/java/com/homework/egms/EgmsApplication.java
@@ -0,0 +1,13 @@
+package com.homework.egms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgmsApplication.class, args);
+    }
+
+}
---
+++ egms/src/test/java/com/homework/egms/EgmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.egms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
